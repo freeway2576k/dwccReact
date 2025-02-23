1b8c010e407e31d270e92f31d864f51f1e023f5d
freeway2576k <freeway2676k@gmail.com>
2025-02-23 17:18:45 +0100

    se crea una pagina de eror404 por si acaso

--- my-gym-shop-front/src/components/NotFound.jsx
+++ my-gym-shop-front/src/components/NotFound.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import './../css/notFound.css'
+import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
+    <main className="d-flex flex-column min-vh-100 justify-content-center">
+      <section className="hero text-center">
+        <div className="container">
+          <h1 className="display-1 fw-bold">404</h1>
+          <h2 className="section-title">¡ALTO FLACOW!</h2>
+          <p className="lead mb-4">
+            Parece que esta página tiene brazo de 35. No te preocupes, tu marido the gym shop está aquí para ayudarte a volver al entrenamiento.
+          </p>
+          <Link to={'/'} className="btn btn-naranja mt-4">Volver al Inicio</Link>
+        </div>
+      </section>
+    </main>
+  );
 }
